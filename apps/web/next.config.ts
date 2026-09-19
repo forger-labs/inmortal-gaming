91a7cc4854cc1bc6969364d7caa96256b4814033
gabotdev <gabrieltorres2014@gmail.com>
2026-09-19 01:53:12 -0400

    fix: cloudflare

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -13,6 +13,10 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
+        hostname: "pub-041a526b56d14b38a39546790a6c9c4c.r2.dev",
+      },
+      {
+        protocol: "https",
         hostname: "pub-a7eeb317de9f489e9add2e3b06002597.r2.dev",
       },
     ],
